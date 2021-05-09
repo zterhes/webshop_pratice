d99b6d304f80da9ebcf808c7aa9feb0c2b291614
zterhes <zterhes1@gmail.com>
2021-05-09 15:10:58 +0200

    create menubar

---
+++ src/components/MenuBar.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const MenuDiv = styled.div`
+    width:100vw;
+    height:5vh;
+    background-color:black;
+`
+
+const MenuBar = () => {
+    return ( 
+
+     );
+}
+ 
+export default MenuBar;
