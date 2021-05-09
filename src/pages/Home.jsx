56efd3be400bb21257643bf71c19edb484eae10e
zterhes <zterhes1@gmail.com>
2021-05-09 15:07:44 +0200

    init and add Home page

---
+++ src/pages/Home.jsx
@@ -0,0 +1,17 @@
+import React from 'react'
+import styled from 'styled-components'
+
+const Page = styled.div`
+    width:100vw;
+    height:100vh;
+`
+
+const Home = () => {
+    return (
+        <Page>
+
+        </Page>
+    );
+}
+
+export default Home;
