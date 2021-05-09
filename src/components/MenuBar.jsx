4bed8274177a39d2aaf129c43fdfc7ff118cd10b
zterhes <zterhes1@gmail.com>
2021-05-09 16:28:08 +0200

    work on navBar

--- src/components/MenuBar.jsx
+++ src/components/MenuBar.jsx
@@ -1,16 +1,68 @@
 import React from 'react'
+import { NavLink } from 'react-router-dom'
 import styled from 'styled-components'
+import logo from '../imgs/sale.png'
+import {ReactComponent as CartIcon} from '../imgs/shopping-cart.svg'
+
 
 const MenuDiv = styled.div`
+    display:flex;
+    justify-content:space-between;
     width:100vw;
-    height:5vh;
+    height:10vh;
     background-color:black;
 `
 
-const MenuBar = () => {
-    return ( 
+const ImgLogo = styled.img`
+    display:flex;
+    max-height:100%;
+    align-self:center;
+`
 
-     );
+const RouterDiv = styled.div`
+    display:flex;
+    align-items:center;
+    justify-content:space-around;
+    height:100%;
+    width:33%;
+`
+
+const RouterButton = styled(NavLink)`
+    display:flex;
+    justify-content:center;
+    align-items:center;
+    height:50%;
+    width:30%;
+    color:white;
+    cursor: pointer;
+    transition: all 0.3s ease 0s; 
+    text-decoration:none;
+    &:hover{
+        background-color:red;
+        width:35%;
+        box-shadow: 0px 15px 20px rgba(46, 229, 157, 0.4);
+    }
+`
+
+const CartIconPic=styled(CartIcon)`
+max-height:70%;
+align-self:center;
+margin-right:10%;
+`
+
+const MenuBar = () => {
+    return (
+        <MenuDiv>
+            <RouterDiv>
+                <RouterButton to="/">Home</RouterButton>           
+                <RouterButton to="/products">Products</RouterButton>
+                <RouterButton to="/about">About</RouterButton>
+            </RouterDiv>
+           <ImgLogo src={logo}/>
+           <CartIconPic fill="white"></CartIconPic>
+
+        </MenuDiv>
+    );
 }
- 
+
 export default MenuBar;
--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,14 +2,17 @@ import React from 'react'
 import styled from 'styled-components'
 
 const Page = styled.div`
+display:flex;
     width:100vw;
     height:100vh;
+    justify-content:center;
+    align-items:center;
 `
 
 const Home = () => {
     return (
         <Page>
-
+            <h1>Welcome in my webshop</h1>
         </Page>
     );
 }
